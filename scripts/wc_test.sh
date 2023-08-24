0b4167e48e1e9da74d16450096cdd6738fe595b7
Nicholas Carrara <ncarrara.physics@gmail.com>
2023-08-24 07:37:21 -0700

    some changes

--- scripts/wc_test.sh
+++ scripts/wc_test.sh
@@ -4,6 +4,7 @@
 #SBATCH --nodelist=wcgpu04
 #SBATCH --gres=gpu:1 
 
+module load apptainer
 HOME=/wclustre/davis_nc/ apptainer shell --nv --home=/wclustre/davis_nc/ /wclustre/davis_nc/images/blip.sif
 BLIP_CONFIG=/work1/davis_nc/Blip/config/blank_config.yaml
 
