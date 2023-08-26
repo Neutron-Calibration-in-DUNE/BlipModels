777a4a6d9a8837b08d21c746bd5e73c8b230cc72
Nicholas Carrara <ncarrara.physics@gmail.com>
2023-08-26 09:43:24 -0700

    adding scripts

--- scripts/wc_docker_install.sh
+++ scripts/wc_docker_install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 module load apptainer
+export APPTAINER_CACHEDIR=/wclustre/davis_nc/apptainer/.apptainer/cache
 apptainer build /wclustre/davis_nc/images/blip.sif docker://infophysics/blip:latest
