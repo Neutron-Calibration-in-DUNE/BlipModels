24282d3d6719d35daa59dc75795740fbddb387f7
Nicholas Carrara <ncarrara.physics@gmail.com>
2023-08-26 09:16:08 -0700

    adding scripts

---
+++ jobs/nersc/example_perlmutter_job.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH -A dune                 # account to use for the job, '--account', '-A'
+#SBATCH -J example              # job name, '--job-name', '-J'
+#SBATCH -C gpu                  # type of job (constraint can be 'cpu' or 'gpu'), '--constraint', '-C'
+#SBATCH -q shared               # Jobs requiring 1 or 2 gpus should use the shared setting, all others use 'regular'
+#SBATCH -t 1:00:00              # amount of time requested for the job, '--time', 't'
+#SBATCH -N 1                    # number of nodes, '--nodes', '-N'
+#SBATCH -n 1                    # number of tasks '--ntasks', -n'
+#SBATCH -c 32                   # number of cores per task, '--cpus-per-task', '-c'
+#SBATCH --gpus-per-task=1       # number of gpus to be used per task
+#SBATCH --gpus-per-node=1       # number of gpus per node.
+#SBATCH --gpu-bind=none         # comment this out if you don't want all gpus visible to each task
+
+# Blip settings
+#SBATCH --image=docker:infophysics/blip:latest  
+#SBATCH --volume="/pscratch/sd/n/ncarrara:/local_scratch;/global/cfs/cdirs/dune/users/ncarrara/blip;/local_blip;/global/cfs/cdirs/dune/users/ncarrara/data;/local_data"
+
+shifter arrakis /local_blip/my_config.yaml
---
+++ scripts/wc_setup_blip.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+# Setup script for installing/running on the Wilson cluster.
+#---------------------Directory---------------------#
+# this handy piece of code determines the relative
+# directory that this script is in.
+SOURCE="${BASH_SOURCE[0]}"
+# resolve $SOURCE until the file is no longer a symlink
+while [ -h "$SOURCE" ]; do 
+  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  # if $SOURCE was a relative symlink, we need to resolve it relative 
+  # to the path where the symlink file was located
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" 
+done
+LOCAL_BLIP_DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )/../"
+
+# You'll need to specify some directories for the anaconda 
+# environment, and where you want the BLIP environment stored.
+export ANACONDA_ENV_DIR=/wclustre/dune/$USER
+export ANACONDA_PKG_DIR=/wclustre/dune/$USER
+export BLIP_ENV_DIR=/wclustre/dune/$USER
+
+# Currently, four modules are required for installation,
+# gnu, openblas, cuda and condaforge.  The versions which
+# work are listed below
+
+GNU_VERSION=gnu8/8.3.0
+OPENBLAS_VERSION=openblas/0.3.7
+CUDA_VERSION=cuda11/11.8.0
+CONDAFORGE_VERSION=condaforge/py39
+
+# First, load the modules
+module load $GNU_VERSION
+module load $OPENBLAS_VERSION
+module load $CUDA_VERSION
+module load $CONDAFORGE_VERSION
+
+# For installing MinkowskiEngine, we need to set
+# the cuda arch list, otherwise it will fail.
+export TORCH_CUDA_ARCH_LIST="3.5;5.0;6.0;6.1;7.0;7.5;8.0;8.6+PTX"
+
+conda activate $BLIP_ENV_DIR
