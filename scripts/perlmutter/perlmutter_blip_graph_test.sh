afaf22be2d09d005c258a6b3b52e6304b631fdb9
Nicholas Carrara <ncarrara.physics@gmail.com>
2023-12-08 07:53:13 -0800

    some changes

--- scripts/perlmutter/perlmutter_blip_graph_test.sh
+++ scripts/perlmutter/perlmutter_blip_graph_test.sh
@@ -2,7 +2,7 @@
 cd /workspace
 
 # download data to /local_data/ and process it
-blip /workspace/BlipModels/blip_graph/config/prep_data.yaml
+blip /workspace/BlipModels/blip_graph/configs/prep_data.yaml
 
 # run the optimize_blip_graph_DEC_1.yaml file
-blip /workspace/BlipModels/blip_graph/config/DEC/optimize_blip_graph_DEC_1.yaml
+blip /workspace/BlipModels/blip_graph/configs/DEC/optimize_blip_graph_DEC_1.yaml
