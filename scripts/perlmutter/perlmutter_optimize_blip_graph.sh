cea9da5d6f4395bab80c8120f5409c618b46c046
Nicholas Carrara <ncarrara.physics@gmail.com>
2023-12-11 07:03:10 -0800

    some changes

--- scripts/perlmutter/perlmutter_optimize_blip_graph.sh
+++ scripts/perlmutter/perlmutter_optimize_blip_graph.sh
@@ -2,7 +2,7 @@
 cd /local_scratch
 
 prep_data_config="/local_blip/BlipModels/blip_graph/configs/prep_data.yaml"
-optimization_config="/local_blip/BlipModels/blip_graph_configs/DEC/optimize_blip_graph_DEC_1.yaml"
+optimization_config="/local_blip/BlipModels/blip_graph/configs/DEC/optimize_blip_graph_DEC_1.yaml"
 optimization_script="/local_blip/BlipModels/scripts/perlmutter_blip_graph_test.slurm"
 hyper_parameter_file="/local_scratch/hyper_parameter_data.csv"
 
