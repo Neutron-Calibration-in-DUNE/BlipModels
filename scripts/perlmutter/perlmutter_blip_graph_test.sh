430da2214309182f53f53add869cfba526e5f2f4
Nicholas Carrara <ncarrara.physics@gmail.com>
2023-12-11 06:10:17 -0800

    creating optimization scripts

--- scripts/perlmutter/perlmutter_blip_graph_test.sh
+++ scripts/perlmutter/perlmutter_blip_graph_test.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
+config_file=$1
 cd /workspace
 
-# download data to /local_data/ and process it
-blip /workspace/BlipModels/blip_graph/configs/prep_data.yaml
-
-# run the optimize_blip_graph_DEC_1.yaml file
-blip /workspace/BlipModels/blip_graph/configs/DEC/optimize_blip_graph_DEC_1.yaml
+# run the config file
+blip config_file
---
+++ scripts/perlmutter/perlmutter_optimize_blip_graph.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+cd /workspace
+
+prep_data_config="/workspace/BlipModels/blip_graph/config/prep_data.yaml"
+optimization_config="/workspace/BlipModels/blip_graph_configs/DEC/optimize_blip_graph_DEC_1.yaml"
+optimization_script="/workspace/BlipModels/scripts/perlmutter_blip_graph_test.slurm"
+hyper_parameter_file="/local_scratch/hyper_parameter_data.csv"
+
+# download data to /local_data/ and process it
+blip "$prep_data_config"
+
+# generate hyper-parameter configs
+create_hyper_parameter_configs "$optimization_config"
+
+# read in the hyper_parameter file
+if [ ! -e "$hyper_parameter_file" ]; then
+    echo "Error: Hyper parameter file '$hyper_parameter_file' not found!"
+    exit 1
+fi
+
+while IFS=',' read -r hyper_parameter_config; do
+    # check that line is not empty
+    if [ -n "$hyper_parameter_config" ]; then
+        sbatch --job-name="$hyper_parameter_config" "$optimization_script" "$hyper_parameter_config"
+    fi
+done < "$hyper_parameter_file"
